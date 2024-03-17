f3757d3d315b248415dfdee1806a8df530c9e79d
DerSaeid <163677878+DerSaeid@users.noreply.github.com>
2024-03-17 11:06:51 +0330

    Update NummerRaten.py
    ich habe diesen Code ins Deutsch übersetz

--- NummerRaten.py
+++ NummerRaten.py
@@ -1,26 +1,26 @@
 from random import randint
 
-shoro = int(input('az che adadi mitonam entekhab konam? '))
-payan = int(input('ta che adadi mitonam entekhab konam? '))
-adad_shansi = randint(shoro, payan)
+start = int(input('Ab welche Nummer kann ich auswählen? '))
+end = int(input('bis welche Nummer kann ich auswählen? '))
+adad_shansi = randint(start, end)
 
 for i in range(1, 6):
-    hads_karbar = int(input('adadi ra ke entekhab kardam hads bezan: '))
+    hads_karbar = int(input('Schätz mal die Nummer, die ich ausgewählt habe : '))
     # yek adad random entekhab shode va karbar an ra hads mizanad
     if hads_karbar < adad_shansi:
         if i == 5:
-            print(f'adadi ke entekhab karde boodam {adad_shansi} bood\n')
+            print(f'die richtige Nummer war {adad_shansi} \n')
         else:
-            print(f'{5 - i} ta joon dige dari')
-            print('yekam boro bala\n')
+            print(f'Du hast noch {5 - i} Chancen')
+            print('Wähle eine größere Zahl\n')
 
     elif hads_karbar > adad_shansi:
         if i == 5:
-            print(f'adadi ke entekhab karde boodam {adad_shansi} bood\n')
+            print(f'die richtige Nummer war {adad_shansi} bood\n')
         else:
-            print(f'{5 - i} ta joon dige dari')
-            print('yekam bia paiin\n')
+            print(f'Du hast noch {5 - i} Chancen')
+            print('Wähle eine kleinere Zahl\n')
 
     else:
-        print('dorost hads zadi hoooooora!!!!')
+        print('ES WAR RICHTICH HOOOOOORRRAAAAA!!!!')
         break
